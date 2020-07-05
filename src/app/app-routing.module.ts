662bfc20ef540d9d2992f3b0533d920d310472f8
Renan Dupas de Matos <renan.matos@serpro.gov.br>
2020-07-05 14:09:15 -0300

    modificações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'listagem',
+    loadChildren: () => import('./listagem/listagem.module').then(m => m.ListagemModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/listagem/listagem.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListagemComponent } from './listagem.component';
+
+
+
+@NgModule({
+  declarations: [ListagemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListagemModule { }
